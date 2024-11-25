dbd583bcc5278e80da9c166ab728b0a38a2a1400
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2024-11-25 15:05:18 -0800

    Add ScrumsView for list of cards

--- ScrumRoll/Views/ScrumsView.swift
+++ ScrumRoll/Views/ScrumsView.swift
@@ -5,3 +5,16 @@
 //  Created by Tiff Kaiser on 11/25/24.
 //
 
+import SwiftUI
+
+struct ScrumsView: View {
+    var body: some View {
+        VStack {
+            Text("ScrumsView Placeholder")
+        }
+    }
+}
+
+#Preview {
+    ScrumsView()
+}
