c26aec3471fbdd1223fc15e37ab19a07233f99fc
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2024-11-25 12:21:58 -0800

    Initial Commit

---
+++ ScrumRoll/ScrumRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumRollApp.swift
+//  ScrumRoll
+//
+//  Created by Tiff Kaiser on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScrumRollTests/ScrumRollTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumRollTests.swift
+//  ScrumRollTests
+//
+//  Created by Tiff Kaiser on 11/25/24.
+//
+
+import Testing
+@testable import ScrumRoll
+
+struct ScrumRollTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
